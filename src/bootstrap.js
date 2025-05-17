492f8251ff0ab67313f6b5917064dc52ffba150a
2019hc04742 <2019hc04742@wilp.bits-pilani.ac.in>
2025-05-17 18:22:08 +0000

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,13 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    firebase: {
+      apiKey: "AIzaSyDPaJ8wB_gRnZhahM6id-KVYBdzL30X5Ew",
+      authDomain: "promptly-c0801.firebaseapp.com",
+      projectId: "promptly-c0801",
+      storageBucket: "promptly-c0801.firebasestorage.app",
+      messagingSenderId: "544780403747",
+      appId: "1:544780403747:web:4f15d723b0827af99c5d96",
+    },
+    
+    
+    geminiApiKey: "AIzaSyB8_Lqn4g1wciqZtiGeDPq1exTox-Bi-O4"
+    };
